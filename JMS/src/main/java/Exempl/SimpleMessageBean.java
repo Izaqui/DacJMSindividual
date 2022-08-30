be59b96c1444565fdc4eb1f029c661384ece2954
Izaqui <izaquiel.silva.ads@gmail.com>
2022-08-30 15:50:19 -0300

    upgrade no codigo

--- JMS/src/main/java/Exempl/SimpleMessageBean.java
+++ JMS/src/main/java/Exempl/SimpleMessageBean.java
@@ -1,2 +1,53 @@
-package Exempl;public class SimpleMessageBean {
+package Exempl;
+
+import com.mysql.cj.protocol.Message;
+import com.mysql.cj.protocol.MessageListener;
+
+import javax.ejb.MessageDriven;
+import javax.ejb.MessageDrivenContext;
+import javax.ejb.ActivationConfigProperty;
+import javax.jms.MessageListener;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+import javax.jms.JMSException;
+import javax.annotation.Resource;
+import java.util.logging.Logger;
+
+
+@MessageDriven(mappedName = "jms/Queue")
+public class SimpleMessageBean implements MessageListener {
+    static final Logger logger = Logger.getLogger("SimpleMessageBean");
+    @Resource
+    private MessageDrivenContext mdc;
+
+    public SimpleMessageBean() {
+    }
+
+    public void onMessage(Message inMessage) {
+        TextMessage msg = null;
+
+        try {
+            if (inMessage instanceof TextMessage) {
+                msg = (TextMessage) inMessage;
+                logger.info("MESSAGE BEAN: Message received: " + msg.getText());
+            } else {
+                logger.warning("Message of wrong type: " + inMessage.getClass().getName());
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+            mdc.setRollbackOnly();
+        } catch (Throwable te) {
+            te.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean processMessage(Message message) {
+        return MessageListener.super.processMessage(message);
+    }
+
+    @Override
+    public void error(Throwable ex) {
+
+    }
 }
--- JMS/src/main/java/Exempl/SimpleMessageClient.java
+++ JMS/src/main/java/Exempl/SimpleMessageClient.java
@@ -1,2 +1,55 @@
-package Exempl;public class SimpleMessageClient {
+package Exempl;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Queue;
+import javax.jms.Connection;
+import javax.jms.Session;
+import javax.jms.MessageProducer;
+import javax.jms.TextMessage;
+import javax.jms.JMSException;
+import javax.annotation.Resource;
+
+
+public class SimpleMessageClient {
+    @Resource(mappedName = "jms/ConnectionFactory")
+    private static ConnectionFactory connectionFactory;
+    @Resource(mappedName = "jms/Queue")
+    private static Queue queue;
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        Session session = null;
+        MessageProducer messageProducer = null;
+        TextMessage message = null;
+        final int NUM_MSGS = 3;
+
+        try {
+            connection = connectionFactory.createConnection();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            messageProducer = session.createProducer(queue);
+            message = session.createTextMessage();
+
+            for (int i = 0; i < NUM_MSGS; i++) {
+                message.setText("This is message " + (i + 1));
+                System.out.println("Sending message: " + message.getText());
+                messageProducer.send(message);
+            }
+
+            System.out.println("Received the messages,");
+            System.out.println(
+                    " check <install_dir>/domains/domain1/logs/server.log.");
+        } catch (JMSException e) {
+            System.out.println("Exception occurred: " + e.toString());
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                }
+            } // if
+
+            System.exit(0);
+        }
+    }
 }
